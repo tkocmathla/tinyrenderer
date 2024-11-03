da12e98eba4a28a9b66135d2b35e9b22d25fcc45
tkocmathla <matt.t.grimm@gmail.com>
2024-11-03 09:54:42 -0700

    add green and blue

--- tgac/tgac.h
+++ tgac/tgac.h
@@ -35,6 +35,8 @@ typedef struct {
 } tgac_pixel_t;
 
 static const tgac_pixel_t TGAC_RED = {.r = 255, .g = 0, .b = 0, .a = 255};
+static const tgac_pixel_t TGAC_GREEN = {.r = 0, .g = 255, .b = 0, .a = 255};
+static const tgac_pixel_t TGAC_BLUE = {.r = 0, .g = 0, .b = 255, .a = 255};
 static const tgac_pixel_t TGAC_BLACK = {.r = 0, .g = 0, .b = 0, .a = 255};
 static const tgac_pixel_t TGAC_WHITE = {.r = 255, .g = 255, .b = 255, .a = 255};
 
